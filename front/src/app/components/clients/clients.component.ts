65650f8d64e6469e52557569e6fb72c8b5a3cd98
Adolfo Blanco <ajb156@outlook.com>
2022-11-24 21:17:09 +0100

    list of clients

--- front/src/app/components/clients/clients.component.ts
+++ front/src/app/components/clients/clients.component.ts
@@ -2,25 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { ClientsService } from '../../services/clients.service';
 import { Client } from '../../interfaces/client';
 
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
-const ELEMENT_DATA: PeriodicElement[] = [
-  { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
-  { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
-  { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
-  { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
-  { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
-  { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
-  { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
-  { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
-  { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
-  { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
-];
-
 @Component({
   selector: 'app-clients',
   templateUrl: './clients.component.html',
@@ -29,17 +10,13 @@ const ELEMENT_DATA: PeriodicElement[] = [
 export class ClientsComponent implements OnInit {
   clients: Client[] = [];
   dataSource = this.clients;
-  displayedColumns: string[] = [
-    'id',
-    'name',
-    'surname1',
-    'email',
-    'createAt',
-    'actions',
-  ];
+  displayedColumns: string[] = ['id', 'name', 'surname1', 'email', 'createAt', 'actions'];
   constructor(private clientService: ClientsService) {}
 
   ngOnInit(): void {
+    /**
+      Obtiene los clientes de la base ded datos
+    */
     this.clientService.getAllClients().subscribe((res) => (this.dataSource = res));
   }
 }
