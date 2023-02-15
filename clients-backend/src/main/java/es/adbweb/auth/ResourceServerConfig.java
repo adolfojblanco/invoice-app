c4cb36b82f315968f7eb2610c4b96e8723dd35cc
AdolfoB <ajb156@outlook.com>
2023-02-15 22:50:32 +0100

    update

--- clients-backend/src/main/java/es/adbweb/auth/ResourceServerConfig.java
+++ clients-backend/src/main/java/es/adbweb/auth/ResourceServerConfig.java
@@ -6,9 +6,7 @@ import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.web.cors.CorsConfiguration;
@@ -22,18 +20,16 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests()
-		.antMatchers("/api/clients/image/**").permitAll()
+		http.authorizeRequests().antMatchers("/api/clients/image/**").permitAll()
 //		.antMatchers(HttpMethod.GET, "/api/clients/**").hasAnyRole("ADMIN, USER")
 //		.antMatchers(HttpMethod.GET, "/api/clients/**").hasRole("ADMIN")
-		.anyRequest().authenticated();
+				.anyRequest().authenticated();
 	}
 
 	@Bean
 	public CorsConfigurationSource corsConfiguationSource() {
 		CorsConfiguration config = new CorsConfiguration();
-		config.setAllowedOrigins(
-				Arrays.asList("http://localhost:4200", "https://invoice.querotemoito.es"));
+		config.setAllowedOrigins(Arrays.asList("http://localhost:4200", "https://invoice.querotemoito.es"));
 		config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
 		config.setAllowCredentials(true);
 		config.setAllowedHeaders(Arrays.asList("Content-Type", "Authorization"));
--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -6,13 +6,11 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
-import { AuthComponent } from './components/auth/auth.component';
+
 import localeEs from '@angular/common/locales/es';
 import { registerLocaleData } from '@angular/common';
 
 import { MaterialModule } from './components/material/material.module';
-import { SharedComponent } from './components/shared/shared.component';
-import { HomeComponent } from './components/home/home.component';
 
 registerLocaleData(localeEs);
 @NgModule({
